c4673f5b8501ad96791578b6d7b56419f57c6897
seomac <icyveins@hotmail.com>
2025-08-03 00:04:20 +0800

    start work on line setting

--- src/termplot/_core.py
+++ src/termplot/_core.py
@@ -3,17 +3,35 @@ import shutil
 class Figure:
     def __init__(
         self,
-        dims: tuple[int, int] = (-1, -1)
+        dims: tuple[int, int] = (-1, -1),
+        withBorder: bool = True
     ):
+        self._withBorder = withBorder
         cols, rows = shutil.get_terminal_size()
         self._dims = (
             dims[0] if dims[0] > 0 else rows,
             dims[1] if dims[1] > 0 else cols
         )
         self._lines = self._make_lines(self._dims)
+        # TODO: i think i should store the drawableROI instead
+        self._drawableDims = self._dims if not self._withBorder else (
+            self._dims[0] - 2, self._dims[1] - 2
+        )
 
     def __repr__(self) -> str:
         return f"<termplot.Figure, dims=({self._dims[0]}, {self._dims[1]})>"
+
+    def setLine(self, row: int, line: list[str]):
+        if len(line) != self._drawableDims[1]:
+            raise ValueError(
+                f"Line must be the same length as the drawable width {self._drawableDims[1]}"
+            )
+
+        # TODO: handle correctly
+        # if row < 0 or row >= self._drawableDims[0]:
+        #     raise ValueError(
+        #         f"Row must be between 0 and the drawable height {self._drawableDims[0]}"
+        #     )
 
     def _make_lines(self, dims: tuple[int, int]) -> list[list]:
         # pre-allocation for size?
@@ -23,7 +41,7 @@ class Figure:
     def stitch(self) -> str:
         return "\n".join(("".join(line) for line in self._lines))
 
-    def drawBorder(self):
+    def _drawBorder(self):
         self._lines[0][0] = "\u256D" # topleft
         self._lines[0][-1] = "\u256E" # topright
         self._lines[-1][0] = "\u2570" # bottomleft
@@ -40,11 +58,12 @@ class Figure:
             self._lines[i][-1] = "\u2502"
 
     def show(self):
+        if self._withBorder:
+            self._drawBorder()
         print(f"{self.stitch()}\n")
 
 if __name__ == "__main__":
     f = Figure()
     print(f)
-    f.drawBorder()
     f.show()
 
